ecb574503e17b7d0c99f94063f617a178cab1a56
songjindian <songjindian1@huawei.com>
2023-05-16 19:18:50 +0800

    want格式修改
    Signed-off-by: songjindian <songjindian1@huawei.com>

--- interfaces/kits/native/want/src/want.cpp
+++ interfaces/kits/native/want/src/want.cpp
@@ -1844,7 +1844,7 @@ bool Want::ReadFromJson(nlohmann::json &wantJson)
         ABILITYBASE_LOGE("Incomplete wantJson");
         return false;
     }
-    
+
     if (!wantJson["deviceId"].is_string()) {
         ABILITYBASE_LOGE("ReadFromJson: deviceId is not a string.");
         return false;
@@ -1858,19 +1858,19 @@ bool Want::ReadFromJson(nlohmann::json &wantJson)
         return false;
     }
     SetElementName(wantJson["deviceId"], wantJson["bundleName"], wantJson["abilityName"]);
-    
+
     if (!wantJson["uri"].is_string()) {
         ABILITYBASE_LOGE("ReadFromJson: uri is not a string.");
         return false;
     }
     SetUri(wantJson["uri"]);
-    
+
     if (!wantJson["type"].is_string()) {
         ABILITYBASE_LOGE("ReadFromJson: type is not a string.");
         return false;
     }
     SetType(wantJson["type"]);
-    
+
     if (!wantJson["flags"].is_number_unsigned()) {
         ABILITYBASE_LOGE("ReadFromJson: flags is not a number.");
         return false;
@@ -1882,7 +1882,7 @@ bool Want::ReadFromJson(nlohmann::json &wantJson)
         return false;
     }
     SetAction(wantJson["action"]);
-    
+
     if (!wantJson["parameters"].is_string()) {
         ABILITYBASE_LOGE("ReadFromJson: parameters is not a string.");
         return false;
