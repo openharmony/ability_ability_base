e7854f248131e36c1658572a09f731eda9b61c60
孙旭辉 <sunxuhui7@huawei.com>
2024-09-03 01:31:42 +0000

    update test/unittest/viewdata/rect_test.cpp.
    Signed-off-by: 孙旭辉 <sunxuhui7@huawei.com>

--- test/unittest/viewdata/rect_test.cpp
+++ test/unittest/viewdata/rect_test.cpp
@@ -25,8 +25,7 @@ using namespace testing::ext;
 namespace OHOS {
 namespace AbilityBase {
 namespace {
-    static const std::string TJSON =
-        "{\"height\":0.0,\"left\":0.0,\"top\":0.0,\"width\":0.0}";
+static const std::string TJSON = "{\"height\":0.0,\"left\":0.0,\"top\":0.0,\"width\":0.0}";
 } // namespace
 class RectTest : public testing::Test {
 public:
