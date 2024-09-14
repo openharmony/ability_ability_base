17cd31e2c2502af4fbeec3c79e45deef328ffca8
chenzexin <chenzexin14@huawei.com>
2024-09-14 15:12:39 +0800

    add parentWindowType
    Signed-off-by: chenzexin <chenzexin14@huawei.com>

--- interfaces/kits/native/session_info/include/session_info.h
+++ interfaces/kits/native/session_info/include/session_info.h
@@ -43,6 +43,7 @@ public:
     std::string identityToken;
     int32_t persistentId = 0;
     uint32_t hostWindowId = 0;
+    uint32_t parentWindowType = 1;
     // Status of the UIAbility invoked by the StartABilityByCall
     CallToState state = CallToState::UNKNOW;
     // Returned resultCode of terminateSelfWithResult
