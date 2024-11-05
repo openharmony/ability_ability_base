d73636129ef2f53908714157c9ad00ec38f57cdb
sunjiakun <sunjiakun5@huawei.com>
2024-11-05 16:20:16 +0800

    upd want
    Signed-off-by: sunjiakun <sunjiakun5@huawei.com>

--- test/fuzztest/wantparamsfifth_fuzzer/wantparamsfifth_fuzzer.cpp
+++ test/fuzztest/wantparamsfifth_fuzzer/wantparamsfifth_fuzzer.cpp
@@ -13,11 +13,11 @@
  * limitations under the License.
  */
 
-#include "wantparamsfour_fuzzer.h"
-
 #include <cstddef>
 #include <cstdint>
 #include <iostream>
+
+#include "wantparamsfifth_fuzzer.h"
 
 #define private public
 #include "want_params.h"
@@ -43,9 +43,10 @@ namespace OHOS {
 namespace {
 constexpr size_t FOO_MAX_LEN = 1024;
 constexpr size_t U32_AT_SIZE = 4;
-constexpr size_t BOOLEAN_TO_STRING = 10;
+constexpr size_t PARAMS_SIZE = 21;
+constexpr int32_t OTHER = -2;
 constexpr int32_t ARRAY_SIZE = 5;
-}
+} // namespace
 
 uint32_t GetU32Data(const char* ptr)
 {
@@ -53,30 +54,11 @@ uint32_t GetU32Data(const char* ptr)
     return (ptr[0] << 24) | (ptr[1] << 16) | (ptr[2] << 8) | ptr[3];
 }
 
-void DoCoverageUnsupportedDataAndWantParams()
-{
-    std::vector<UnsupportedData> cachedUnsupportedData;
-    UnsupportedData data1;
-    cachedUnsupportedData.push_back(data1);
-    UnsupportedData data2(std::move(data1));
-    cachedUnsupportedData.push_back(data2);
-    UnsupportedData data3;
-    cachedUnsupportedData.push_back(data3);
-    UnsupportedData data4 = data3;
-    cachedUnsupportedData.push_back(data4);
-    data3 = data4;
-    data4 = std::move(data3);
-    WantParams wantOther;
-    std::shared_ptr<WantParams> wantParams = std::make_shared<WantParams>(std::move(wantOther));
-    wantParams->SetCachedUnsupportedData(cachedUnsupportedData);
-    wantParams->GetCachedUnsupportedData(cachedUnsupportedData);
-}
-
-void ConvertBasicDataTypes(const char* data, size_t size, std::map<std::string, sptr<IInterface>>& params)
+void WriteBasicTypesToParcel(Parcel& parcel, const char* data, size_t size)
 {
     std::string stringValue(data, size);
     bool boolValue = static_cast<bool>(GetU32Data(data));
-    uint8_t byteValue = static_cast<uint8_t>(GetU32Data(data));
+    int8_t byteValue = static_cast<int8_t>(GetU32Data(data));
     char charValue = static_cast<char>(GetU32Data(data));
     int16_t shortValue = static_cast<int16_t>(GetU32Data(data));
     int32_t intValue = static_cast<int32_t>(GetU32Data(data));
@@ -84,19 +66,47 @@ void ConvertBasicDataTypes(const char* data, size_t size, std::map<std::string, 
     float floatValue = static_cast<float>(GetU32Data(data));
     double doubleValue = static_cast<double>(GetU32Data(data));
 
-    params["string"] = String::Box(stringValue);
-    params["boolean"] = Boolean::Box(boolValue);
-    params["byte"] = Byte::Box(byteValue);
-    params["char"] = Char::Box(charValue);
-    params["short"] = Short::Box(shortValue);
-    params["integer"] = Integer::Box(intValue);
-    params["long"] = Long::Box(longValue);
-    params["float"] = Float::Box(floatValue);
-    params["double"] = Double::Box(doubleValue);
+    parcel.WriteInt32(PARAMS_SIZE);
+    parcel.WriteString16(Str8ToStr16("booleanKey"));
+    parcel.WriteInt32(WantParams::VALUE_TYPE_BOOLEAN);
+    parcel.WriteBool(boolValue);
+    parcel.WriteString16(Str8ToStr16("byteKey"));
+    parcel.WriteInt32(WantParams::VALUE_TYPE_BYTE);
+    parcel.WriteInt8(byteValue);
+    parcel.WriteString16(Str8ToStr16("charKey"));
+    parcel.WriteInt32(WantParams::VALUE_TYPE_CHAR);
+    parcel.WriteInt32(charValue);
+    parcel.WriteString16(Str8ToStr16("shortKey"));
+    parcel.WriteInt32(WantParams::VALUE_TYPE_SHORT);
+    parcel.WriteInt16(shortValue);
+    parcel.WriteString16(Str8ToStr16("intKey"));
+    parcel.WriteInt32(WantParams::VALUE_TYPE_INT);
+    parcel.WriteInt32(intValue);
+    parcel.WriteString16(Str8ToStr16("longKey"));
+    parcel.WriteInt32(WantParams::VALUE_TYPE_LONG);
+    parcel.WriteInt64(longValue);
+    parcel.WriteString16(Str8ToStr16("floatKey"));
+    parcel.WriteInt32(WantParams::VALUE_TYPE_FLOAT);
+    parcel.WriteFloat(floatValue);
+    parcel.WriteString16(Str8ToStr16("doubleKey"));
+    parcel.WriteInt32(WantParams::VALUE_TYPE_DOUBLE);
+    parcel.WriteDouble(doubleValue);
+    parcel.WriteString16(Str8ToStr16("stringKey"));
+    parcel.WriteInt32(WantParams::VALUE_TYPE_STRING);
+    parcel.WriteString16(Str8ToStr16(stringValue));
 }
 
-void CreateArrays(const char* data, size_t size, std::map<std::string, sptr<IInterface>>& params)
+void WriteArrayToParcel(
+    Parcel& parcel, const std::string& key, int32_t type, sptr<IArray> array, std::shared_ptr<WantParams> wantParams)
 {
+    parcel.WriteString16(Str8ToStr16(key));
+    parcel.WriteInt32(type);
+    wantParams->WriteArrayToParcel(parcel, array, 0);
+}
+
+void WriteArraysToParcel(Parcel& parcel, const char* data, size_t size, std::shared_ptr<WantParams> wantParams)
+{
+    std::string stringValue(data, size);
     sptr<IArray> booleanArray = new Array(ARRAY_SIZE, g_IID_IBoolean);
     sptr<IArray> charArray = new Array(ARRAY_SIZE, g_IID_IChar);
     sptr<IArray> byteArray = new Array(ARRAY_SIZE, g_IID_IByte);
@@ -108,8 +118,6 @@ void CreateArrays(const char* data, size_t size, std::map<std::string, sptr<IInt
     sptr<IArray> stringArray = new Array(ARRAY_SIZE, g_IID_IString);
     sptr<IArray> wantParamsArray = new Array(ARRAY_SIZE, g_IID_IWantParams);
     sptr<IArray> other = new Array(ARRAY_SIZE, g_IID_IRemoteObjectWrap);
-    
-    std::string stringValue(data, size);
     for (int32_t i = 0; i < ARRAY_SIZE; i++) {
         booleanArray->Set(i, Boolean::Box(static_cast<bool>(GetU32Data(data))));
         charArray->Set(i, Char::Box(static_cast<char>(GetU32Data(data))));
@@ -122,87 +130,37 @@ void CreateArrays(const char* data, size_t size, std::map<std::string, sptr<IInt
         stringArray->Set(i, String::Box(stringValue));
         other->Set(i, RemoteObjectWrap::Box(nullptr));
     }
-
-    params["booleanArray"] = booleanArray;
-    params["charArray"] = charArray;
-    params["byteArray"] = byteArray;
-    params["shortArray"] = shortArray;
-    params["integerArray"] = integerArray;
-    params["longArray"] = longArray;
-    params["floatArray"] = floatArray;
-    params["doubleArray"] = doubleArray;
-    params["stringArray"] = stringArray;
-    params["wantParamsArray"] = wantParamsArray;
-    params["other"] = other;
-}
-
-void HandleWantParams(std::map<std::string, sptr<IInterface>>& params)
-{
-    WantParams source;
-    source.params_ = params;
-    WantParams dest(source);
-    sptr<IArray> destNull = nullptr;
-    if (source == dest) {}
-    dest.NewArrayData(static_cast<IArray*>(params["booleanArray"].GetRefPtr()), destNull);
-    
-    Parcel parcel;
-    UnsupportedData unsupportedData;
-    dest.cachedUnsupportedData_.push_back(unsupportedData);
-    dest.DoMarshalling(parcel, 0);
-    
-    WantParams paramsClose(dest);
-    WantParams paramsRemove(dest);
-    WantParams paramsDup(dest);
-    paramsClose.CloseAllFd();
-    paramsRemove.RemoveAllFd();
-    paramsDup.DupAllFd();
-}
-
-void DoCoverageNewArrayData(const char* data, size_t size)
-{
-    std::map<std::string, sptr<IInterface>> params;
-    ConvertBasicDataTypes(data, size, params);
-    CreateArrays(data, size, params);
-    HandleWantParams(params);
-}
-
-void DoCoverageGetInterfaceByType()
-{
-    std::string stringValue = std::to_string(BOOLEAN_TO_STRING);
-    WantParams params;
-    for (size_t i = 0; i < BOOLEAN_TO_STRING; i++) {
-        params.GetInterfaceByType(i, stringValue);
-    }
-    params.GetInterfaceByType(WantParams::VALUE_TYPE_ARRAY, stringValue);
-}
-
-void DoCoverageWriteToParce(const char* data, size_t size)
-{
-    Parcel parcel;
-    std::string stringValue(data, size);
-    bool boolValue = static_cast<bool>(GetU32Data(data));
-    int intValue = static_cast<int>(GetU32Data(data));
-    long longValue = static_cast<long>(GetU32Data(data));
-    sptr<IInterface> stringIt = String::Box(stringValue);
-    sptr<IInterface> boolIt = Boolean::Box(boolValue);
-    sptr<IInterface> intIt = Integer::Box(intValue);
-    sptr<IInterface> longIt = Long::Box(longValue);
-    WantParams params;
-    params.WriteToParcelString(parcel, stringIt);
-    params.WriteToParcelBool(parcel, boolIt);
-    params.WriteToParcelInt(parcel, intIt);
-    params.WriteToParcelLong(parcel, longIt);
+    WriteArrayToParcel(parcel, "stringArray", WantParams::VALUE_TYPE_STRINGARRAY, stringArray, wantParams);
+    WriteArrayToParcel(parcel, "booleanArray", WantParams::VALUE_TYPE_BOOLEANARRAY, booleanArray, wantParams);
+    WriteArrayToParcel(parcel, "byteArray", WantParams::VALUE_TYPE_BYTEARRAY, byteArray, wantParams);
+    WriteArrayToParcel(parcel, "charArray", WantParams::VALUE_TYPE_CHARARRAY, charArray, wantParams);
+    WriteArrayToParcel(parcel, "shortArray", WantParams::VALUE_TYPE_SHORTARRAY, shortArray, wantParams);
+    WriteArrayToParcel(parcel, "integerArray", WantParams::VALUE_TYPE_INTARRAY, integerArray, wantParams);
+    WriteArrayToParcel(parcel, "longArray", WantParams::VALUE_TYPE_LONGARRAY, longArray, wantParams);
+    WriteArrayToParcel(parcel, "floatArray", WantParams::VALUE_TYPE_FLOATARRAY, floatArray, wantParams);
+    WriteArrayToParcel(parcel, "doubleArray", WantParams::VALUE_TYPE_DOUBLEARRAY, doubleArray, wantParams);
+    WriteArrayToParcel(parcel, "wantParamsArray", WantParams::VALUE_TYPE_WANTPARAMSARRAY, wantParamsArray, wantParams);
+    WriteArrayToParcel(parcel, "other", OTHER, other, wantParams);
+    parcel.WriteInt32(WantParams::VALUE_TYPE_NULL);
 }
 
 bool DoSomethingInterestingWithMyAPI(const char* data, size_t size)
 {
-    DoCoverageUnsupportedDataAndWantParams();
-    DoCoverageNewArrayData(data, size);
-    DoCoverageGetInterfaceByType();
-    DoCoverageWriteToParce(data, size);
+    WantParams wantOther;
+    std::shared_ptr<WantParams> wantParams = std::make_shared<WantParams>(wantOther);
+
+    Parcel parcel;
+    WriteBasicTypesToParcel(parcel, data, size);
+    WriteArraysToParcel(parcel, data, size, wantParams);
+
+    auto wantPtr = wantParams->Unmarshalling(parcel, 0);
+    if (wantPtr) {
+        delete wantPtr;
+        wantPtr = nullptr;
+    }
     return true;
 }
-}
+} // namespace OHOS
 
 /* Fuzzer entry point */
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
@@ -218,7 +176,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
         return 0;
     }
 
-    char* ch = (char *)malloc(size + 1);
+    char* ch = (char*)malloc(size + 1);
     if (ch == nullptr) {
         std::cout << "malloc failed." << std::endl;
         return 0;
