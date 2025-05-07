b63a5841c73b82bfba46cb32add8df46230dfc66
Lby <liboyu12@huawei.com>
2025-05-07 21:12:07 +0800

    添加TransformToCWant
    Signed-off-by: Lby <liboyu12@huawei.com>

--- interfaces/kits/c/cwant/src/want_manager.cpp
+++ interfaces/kits/c/cwant/src/want_manager.cpp
@@ -69,5 +69,93 @@ namespace AAFwk {
     want.SetParams(wantParams);
     return ABILITY_BASE_ERROR_CODE_NO_ERROR;
 }
+
+constexpr int32_t FD_NULL = -1;
+constexpr int32_t FD_SIZE = 2;
+
+std::pair<std::string, int> GetFD(std::pair<std::string, sptr<IInterface>> fdparams)
+{
+    std::pair<std::string, int> fd = {"", FD_NULL};
+    ABILITYBASE_LOGD("params key: %{public}s", fdparams.first.c_str());
+    auto wantParams = WantParamWrapper::Unbox(IWantParams::Query(fdparams.second));
+    auto params = wantParams.GetParams();
+    if (params.size() != FD_SIZE) {
+        ABILITYBASE_LOGD("params key: %{public}s not fd", fdparams.first.c_str());
+        return fd;
+    }
+
+    auto typeIt = params.find(TYPE_PROPERTY);
+    if (typeIt == params.end()) {
+        ABILITYBASE_LOGD("params key: %{public}s not find FD_TYPE", fdparams.first.c_str());
+        return fd;
+    }
+    IString *strValue = IString::Query(typeIt->second);
+    if (strValue == nullptr) {
+        ABILITYBASE_LOGD("params key: %{public}s FD_TYPE not string", fdparams.first.c_str());
+        return fd;
+    }
+    std::string typeString = String::Unbox(strValue);
+    if (typeString != FD) {
+        ABILITYBASE_LOGD("params key: %{public}s not FD", fdparams.first.c_str());
+        return fd;
+    }
+
+    auto valueIt = params.find(VALUE_PROPERTY);
+    if (valueIt == params.end()) {
+        ABILITYBASE_LOGD("params key: %{public}s not find FD_VALUE", fdparams.first.c_str());
+        return fd;
+    }
+    IInteger *intValue = IInteger::Query(valueIt->second);
+    if (intValue == nullptr) {
+        ABILITYBASE_LOGD("params key: %{public}s FD_VALUE not int", fdparams.first.c_str());
+        return fd;
+    }
+    ABILITYBASE_LOGD("params key: %{public}s, value: %{public}d", fdparams.first.c_str(), Integer::Unbox(intValue));
+    fd = {fdparams.first, Integer::Unbox(intValue)};
+    return fd;
+}
+
+[[maybe_unused]] AbilityBase_ErrorCode CWantManager::TransformToCWantWithoutElement(
+    Want want, bool isDup, AbilityBase_Want &cwant)
+{
+    cwant.uri = want.GetUriString();
+    WantParams wantParams = want.GetParams();
+    auto params = wantParams.GetParams();
+    for (const auto& iter : params) {
+        if (IString::Query(iter.second) != nullptr) {
+            ABILITYBASE_LOGD("params key: %{public}s, value: %{public}s",
+                iter.first.c_str(),
+                String::Unbox(IString::Query(iter.second)).c_str());
+            cwant.params.insert(
+                std::pair<std::string, std::string>(iter.first, String::Unbox(IString::Query(iter.second))));
+        } else if (IInteger::Query(iter.second) != nullptr) {
+            ABILITYBASE_LOGD("params key: %{public}s, value: %{public}d",
+                iter.first.c_str(),
+                Integer::Unbox(IInteger::Query(iter.second)));
+            cwant.intParams.insert(
+                std::pair<std::string, int>(iter.first, Integer::Unbox(IInteger::Query(iter.second))));
+        } else if (IBoolean::Query(iter.second) != nullptr) {
+            ABILITYBASE_LOGD("params key: %{public}s, value: %{public}d",
+                iter.first.c_str(),
+                Boolean::Unbox(IBoolean::Query(iter.second)));
+            cwant.boolParams.insert(
+                std::pair<std::string, bool>(iter.first, Boolean::Unbox(IBoolean::Query(iter.second))));
+        } else if (IDouble::Query(iter.second) != nullptr) {
+            ABILITYBASE_LOGD("params key: %{public}s, value: %{public}lf",
+                iter.first.c_str(),
+                Double::Unbox(IDouble::Query(iter.second)));
+            cwant.doubleParams.insert(
+                std::pair<std::string, double>(iter.first, Double::Unbox(IDouble::Query(iter.second))));
+        } else if (IWantParams::Query(iter.second) != nullptr) {
+            auto fd = GetFD(iter);
+            if (fd.first != iter.first || fd.second == FD_NULL) {
+                continue;
+            }
+            cwant.fds.insert(fd);
+        }
+    }
+
+    return ABILITY_BASE_ERROR_CODE_NO_ERROR;
+}
 } // namespace AAFwk
 } // namespace OHOS
